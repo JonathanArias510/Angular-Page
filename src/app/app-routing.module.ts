a177a71975d488fb2474a49fb6807a7c3f99f7e1
Jonathan Arias <andresarias510@gmail.com>
2018-11-01 15:28:52 -0500

    first Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { JobComponent } from './job/job.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "jobs", component: JobComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "**", component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
